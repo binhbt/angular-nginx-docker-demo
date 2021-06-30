c2c4dfd5fa6f9a992c07d8bfa12cec5455ff379c
leobui <thanhbinh.gd@gmail.com>
2021-06-30 21:04:56 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aston-villa-app'`, () => {
+  it(`should have as title 'web-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('aston-villa-app');
+    expect(app.title).toEqual('web-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aston-villa-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-app!');
   });
 });
